4a9e776606fe21a5ae60cb5abf1456e9dc0b2e4e
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-02-14 17:58:10 -0500

    Update online.js

--- online.js
+++ online.js
@@ -1,1 +1,13 @@
-
+function download(p1,p2) {
+var el = document.createElement("A");
+    el.setAttribute("href","data:application,"+p1);
+    el.setAttribute("download",p2);    
+    if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        el.dispatchEvent(event);
+    }
+    else {
+        el.click();
+    }
+}
