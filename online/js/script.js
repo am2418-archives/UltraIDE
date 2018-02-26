1ca5d0671dcad84168da8e9eb8e3c6a00c53f0cb
MasterOfTheTiger <ted.programmer@gmail.com>
2018-02-26 11:30:01 -0800

    Use ultralang

--- online/js/script.js
+++ online/js/script.js
@@ -1,8 +1,6 @@
-var code = document.getElementsByTagName('textarea')[0].value;
-var render = document.getElementById('render');
-
 function update() {
-    code = document.getElementsByTagName('textarea')[0].value;
-    render.innerHTML = code;
+    console.log(u("textarea").content[0].value);
+    var code = document.getElementsByTagName('textarea')[0].value;
+    u("#render").set(code);
 }
 u('button').on('click', update);
